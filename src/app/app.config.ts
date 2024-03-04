b7d9ed68be7ebe1e9ede25e8fb9f0b8732fcd2e2
pumitle <65011212026@msu.ac.th>
2024-03-04 23:54:22 +0700

    nongtle 4/3 /67

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(),
-    provideHttpClient()]
+    provideHttpClient(), provideAnimationsAsync()]
 };
