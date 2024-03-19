4339d0c12f01b091f4bbc2f61148208d8a4d9ac8
WYEYOU <65011212028@msu.ac.th>
2024-03-19 07:24:52 +0700

    wye/19/7:00 AM

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -4,20 +4,21 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 import { MysqlService } from '../app/../../mysql.service'; 
 import { HttpClient, HttpClientModule } from '@angular/common/http'; 
 import { UserResponese,UploadRes } from '../../Modeldatabase/user_get';
-import { lastValueFrom } from 'rxjs';
 import {MatButtonModule} from '@angular/material/button';
 import {Router } from '@angular/router';
 import { AuthenticationService } from '../../authen.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLink,HttpClientModule,MatButtonModule],
+  imports: [CommonModule,RouterOutlet,RouterLink,HttpClientModule,MatButtonModule,MatFormFieldModule,MatInputModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
   uid: any;
   user : UserResponese | undefined;
   data: UploadRes[] = [];
@@ -85,5 +86,30 @@ async loadDataAsync() {
   console.log("data is ", this.data);
   }
 
+}
+
+openPopup() {
+  document.getElementById('myModal')!.classList.remove('hide');
+  document.getElementById('myModal')!.classList.add('show');
+  document.getElementById('myModal')!.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+}
+
+async closePopup() {
+  document.body.style.overflow = 'auto'; // อนิเมชันเสร็จสิ้น ปรับ overflow เพื่อเปลี่ยนเป็น auto
+  const modal = document.getElementById('myModal')!;
+  modal.classList.add('hide'); // เพิ่มคลาส "hide" เพื่อเล่นอนิเมชัน fadeOut
+  await new Promise(resolve => setTimeout(resolve, 1000)); // รอให้อนิเมชันเสร็จสิ้น (300 มิลลิวินาที)
+  modal.style.display = 'none'; // ซ่อนป๊อปอัพ
+}
+
+imageSrc: string | undefined;
+file? : File;
+previewImage(event: Event) {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files.length > 0) {
+    this.file = target.files[0];
+    this.imageSrc = URL.createObjectURL(this.file);
+  }
 }
 }
